050deece0252779bb4503cdad39584a60a86f011
Anjith54 <anjithachu54@gmail.com>
2024-06-28 11:09:10 +0530

    hg

--- app/src/main/java/com/logi/entrylog/MainActivity.java
+++ app/src/main/java/com/logi/entrylog/MainActivity.java
@@ -1,14 +1,33 @@
 package com.logi.entrylog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2;
+    AppCompatButton b1;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.Uname);
+        ed2=(EditText) findViewById(R.id.Pass);
+        b1=(AppCompatButton) findViewById(R.id.Log);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUsername=ed1.getText().toString();
+                Toast.makeText(getApplicationContext(),getUsername,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
